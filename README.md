673f9ceba833fefaafccd0c02d506a7591e3f848
natleo <n8wyliu@gmail.com>
2023-04-05 12:23:30 -0700

    Inital commit, Swallows Return and Wild Geese Fly North #1

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a cool site for cool people. 
---
+++ website/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>[1] Swallows Return and Wild Geese Fly North</h1>
+      <h1>Day 1/7 Ends 4/11</h1>
+      <p>Epoch 1 is characterized by the beginning of the Spring semester at UCSC. 
+        With a 15 unit courseload of all Upper Division Courses: Data Structures 
+        and Algorithms CSE101, Intro to Compilers CSE110A, and Computer Architecture
+        CSE120, Epoch 1 represents the beginning of a very difficult Spring Semester 
+        and a few key decisions:</p>
+      <ul>
+        <li>Beginning to digitalize my journals</li>
+        <li>Dropping ASTR3: Planetary Systems, a fun course, for CSE110A, a necessary 
+          course</li>
+        <li>The decision to have a difficult courseload</li>
+      </ul>
+      <p>The underlying assumptions of the key decisions is that we've already got 
+        experience with CSE101 and CSE120, as well as the structure of the classes: 
+        Two are mostly projects based. But the most important is the professors: 
+        Tyler Sorensen (CSE110A), Patrick Tantalo (CSE101), and Sagnik Nath (CSE120)
+        have the highest rate my professor scores, and people have generally loved 
+        taking these professors.</p>
+      <h3>Objective: Personal Growth</h3>
+      <p>Personal Growth is measured by the efforts placed in your chosen field given the epoch.</p>
+      <h3>Key Results:</h3>
+      <ul>
+        <li>(CSE101) Programming Assignment #1 Due 4/11</li>
+        <li>(CSE120) Syllabus Acknowledgement Due 4/11</li>
+        <li>0/15 Hours of CSE110A</li>
+        <li>1/3 Lectures Attended</li>
+        <li>0/15 Hours of CSE101</li>
+        <li>0/2 Lectures Attended</li>
+        <li>0/15 Hours of CSE120</li>
+        <li>0/2 Lectures Attended</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
